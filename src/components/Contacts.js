6b691ccfa96a3c80c4145631c5be2164d80666d8
rapoologitec <guangrongpxj@gmail.com>
2021-10-16 15:13:03 +0800

    filtering contacts feature

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -3,20 +3,24 @@ import {EndPointContext} from "./App";
 import TheNavbar from "./TheNavbar";
 import ActivityItem from "./ActivityItem";
 import Button from "react-bootstrap/Button";
-import {Row} from "react-bootstrap";
+import {Col, Form, Row} from "react-bootstrap";
 import {Link} from "react-router-dom";
 import ContactItem from "./ContactItem";
 import Footer from "./Footer";
 
 function Contacts(){
     const URLEndContext = useContext(EndPointContext)
-    const  getDashURL = URLEndContext + '/contact/all'
+    const getDashURL = URLEndContext + '/contact/all'
+    const filterURL = URLEndContext + '/contact/'
     const CARD_WIDTH = 400
 
     const [redirect, setRedirect] = useState(false)
     const [loading, setLoading] = useState(true)
     const [list, setList] = useState([])
     const [numRow, setNumRow] = useState(Math.floor(window.innerWidth / CARD_WIDTH))
+    const [isFiltered, setIsFiltered] = useState(false)
+    const [filterCompany, setFilterCompany] = useState("")
+    const [filteredList, setFilteredList] = useState([])
 
     const [width, setWidth] = React.useState(window.innerWidth);
 
@@ -56,6 +60,37 @@ function Contacts(){
 
 
     function printList(){
+        if (isFiltered){
+            if (filteredList.length === 0){
+                return(
+                    <div>
+                        <h3>You have no Contacts from the company {filterCompany}</h3>
+                        <br/>
+                        <Button size={"sm"} variant={"warning"} onClick={clearFilter}>Clear Filter</Button>
+                        <br/>
+                    </div>
+
+                )
+            }
+            else{return(
+                <div>
+                    <p>Showing Results for {filterCompany}</p>
+                    <Button size={"sm"} variant={"warning"} onClick={clearFilter}>Clear Filter</Button>
+                    <br/>
+                    <br/>
+                    <Row xs={1} md={numRow} className="g-4">
+                        {
+                            filteredList.map(contact => {
+                                return(
+                                    <ContactItem contact={contact}/>
+                                )
+                            })
+                        }
+                    </Row>
+                </div>
+            )
+            }
+        }
         if (loading){
             return(
                 <h3>Loading...</h3>
@@ -82,6 +117,30 @@ function Contacts(){
         )
     }
 
+    function clearFilter(){
+        setFilteredList([])
+        setFilterCompany("")
+        setIsFiltered(false)
+    }
+
+    function handleFilter(){
+        fetch(filterURL + filterCompany, {method:'GET', credentials:'include', headers: {
+                'Content-Type': 'application/json'
+            }}).then
+        (res=>{
+                if (res.status !== 200) throw new Error("cannot filter contacts")
+                return res.json()
+            }
+
+        ).then(data => {
+            setFilteredList(data)
+            setIsFiltered(true)
+            console.log(JSON.stringify(data))
+            return data
+        }).catch(err => console.log(err))
+        console.log(filterCompany)
+    }
+
     return(
         <div className = "dashboard-background-style">
         <TheNavbar/>
@@ -92,6 +151,29 @@ function Contacts(){
             <br/>
             <Link to={"/contacts/new"}><Button>+ Create New Contact</Button></Link>
             <br/>
+            <br/>
+            <Form>
+                <Row className="align-items-center">
+                    <Col xs="auto">
+                        <Form.Label htmlFor="inlineFormInput" visuallyHidden>
+                            Name
+                        </Form.Label>
+
+                        <Form.Control
+                            className="mb-2"
+                            id="Filtering"
+                            placeholder="Filter by Company"
+                            value={filterCompany}
+                            onChange={(e) => setFilterCompany(e.target.value)}
+                        />
+                    </Col>
+                    <Col xs="auto">
+                        <Button onClick={handleFilter}>
+                            Filter
+                        </Button>
+                    </Col>
+                </Row>
+            </Form>
             <br/>
             {printList()}
         </div>
